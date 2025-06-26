a5cb711bcb0ce5139a67388d86d8a2bf141d53de
gnacuzzi <gnacuzzi@itba.edu.ar>
2025-06-26 12:38:05 -0300

    fix issue on greeting read

--- src/server/socks5.c
+++ src/server/socks5.c
@@ -34,55 +34,47 @@ static void on_greet(const unsigned state, struct selector_key *key) {
 }
 
 static unsigned on_greet_read(struct selector_key *key) {
-    socks5_session  *s = key->data;
+    socks5_session *s = key->data;
     socks5_greeting *g = &s->parsers.greeting;
-    buffer          *buf = &s->c2p_read;
+    buffer *buf = &s->c2p_read;
 
-    size_t  avail;
+    size_t space;
+    uint8_t *dst = buffer_write_ptr(buf, &space);
+    ssize_t r = recv(key->fd, dst, space, 0);
+    if (r <= 0) {
+        return SOCKS5_CLOSING;
+    }
+    buffer_write_adv(buf, r);
+
+    size_t avail;
     uint8_t *ptr = buffer_read_ptr(buf, &avail);
-
     while (avail > 0) {
         uint8_t b = *ptr;
         switch (g->bytes_read) {
           case 0:
-            g->req.version = b;
-            if (g->req.version != 0x05) {
-                return SOCKS5_ERROR;
-            }
+            if ((g->req.version = b) != 0x05)  return SOCKS5_ERROR;
             break;
           case 1:
-            g->req.nmethods = b;
-            if (g->req.nmethods == 0) {
-                return SOCKS5_ERROR;
-            }
+            if ((g->req.nmethods = b) == 0)    return SOCKS5_ERROR;
             break;
           default:
-            if ((g->bytes_read - 2) < g->req.nmethods) {
+            if (g->bytes_read - 2 < g->req.nmethods)
                 g->req.methods[g->bytes_read - 2] = b;
-            }
             break;
         }
-
         g->bytes_read++;
-        buffer_read_adv(buf, 1);
-        ptr++; avail--;
+        buffer_read_adv(buf, 1);  ptr++;  avail--;
 
-        if (g->bytes_read == (size_t)(2 + g->req.nmethods)) {
+        if (g->bytes_read == 2 + g->req.nmethods) {
             g->rep.version = 0x05;
             g->rep.method = NO_ACCEPTABLE_METHODS;
             for (uint8_t i = 0; i < g->req.nmethods; i++) {
-                if (g->req.methods[i] == NO_AUTH) {
-                    g->rep.method = NO_AUTH;
-                    break;
-                }
-                if (g->req.methods[i] == USER_PASS) {
-                    g->rep.method = USER_PASS;
-                }
+              if (g->req.methods[i] == NO_AUTH) { g->rep.method = NO_AUTH; break; }
+              if (g->req.methods[i] == USER_PASS) g->rep.method = USER_PASS;
             }
             uint8_t reply[2] = { g->rep.version, g->rep.method };
-            for (size_t i = 0; i < sizeof(reply); i++) {
+            for (size_t i = 0; i < sizeof(reply); i++)
                 buffer_write(&s->p2c_write, reply[i]);
-            }
             selector_set_interest_key(key, OP_WRITE);
             return (g->rep.method == NO_ACCEPTABLE_METHODS)
                    ? SOCKS5_ERROR
@@ -90,8 +82,9 @@ static unsigned on_greet_read(struct selector_key *key) {
         }
     }
 
-    return SOCKS5_GREETING;
+    return SOCKS5_GREETING;  
 }
+
 
 static unsigned on_greet_write(struct selector_key *key) {
     socks5_session *s = key->data;
@@ -99,7 +92,7 @@ static unsigned on_greet_write(struct selector_key *key) {
     size_t n; uint8_t *ptr;
 
     ptr = buffer_read_ptr(buf, &n);
-    ssize_t sent = send(key->fd, ptr, n, MSG_NOSIGNAL);
+    ssize_t sent = send(key->fd, ptr, n, MSG_NOSIGNAL); //no es bloquante no?
     if (sent <= 0) {
         return SOCKS5_CLOSING;
     }

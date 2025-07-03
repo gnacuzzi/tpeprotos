02fd439f72ebc19c0f28c40e5b1ab9739441e62b
pilarfrutos <pfrutos@itba.edu.ar>
2025-07-02 23:36:12 -0300

    Cleared out most warnings

--- src/server/metp/metrics.c
+++ src/server/metp/metrics.c
@@ -4,34 +4,34 @@ static uint32_t historic_connections;
 static uint32_t socks_current_connections;
 static uint64_t bytes_transferred;
 
-void init_metrics() {
+void init_metrics(void) {
     historic_connections = 0;
     socks_current_connections = 0;
     bytes_transferred = 0;
 }
 
-uint32_t get_historic_connections() {
+uint32_t get_historic_connections(void) {
     return historic_connections;
 }
 
-uint32_t get_socks_current_connections() {
+uint32_t get_socks_current_connections(void) {
     return socks_current_connections;
 }
 
-uint64_t get_bytes_transferred() {
+uint64_t get_bytes_transferred(void) {
     return bytes_transferred;
 }
 
 //TODO: ver desde donde llamamos a estas funciones
-void increment_historic_connections() {
+void increment_historic_connections(void) {
     historic_connections++;
 }
 
-void increment_current_connections() {
+void increment_current_connections(void) {
     socks_current_connections++;
 }
 
-void decrement_current_connections() {
+void decrement_current_connections(void) {
     if (socks_current_connections > 0) {
         socks_current_connections--;
     }
--- src/server/metp/metrics.h
+++ src/server/metp/metrics.h
@@ -1,16 +1,16 @@
 #ifndef METRICTS_H
-#define METRICS_H
+#define METRICTS_H
 
 #include <stdint.h>
 
-void init_metrics();
-uint32_t get_historic_connections();
-uint32_t get_socks_current_connections();
-uint64_t get_bytes_transferred();
+void init_metrics(void);
+uint32_t get_historic_connections(void);
+uint32_t get_socks_current_connections(void);
+uint64_t get_bytes_transferred(void);
 
-void increment_historic_connections();
-void increment_current_connections();
-void decrement_current_connections();
+void increment_historic_connections(void);
+void increment_current_connections(void);
+void decrement_current_connections(void);
 void add_bytes_transferred(uint64_t bytes);
 
 #endif
